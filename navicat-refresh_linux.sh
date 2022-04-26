a0615fbc286d6cdc9e0336ce63d14c947eebe863
cubone <cuiv011@foxmail.com>
2022-04-26 20:37:12 +0800

    first commit

---
+++ navicat-refresh_linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RED="\e[1;31m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED}"
+echo -e "======================================="
+echo -e "|           !!! WARNING !!!           |"
+echo -e "======================================="
+echo -e "| All data can be destroy.            |"
+echo -e "| Always backup before continue.      |"
+echo -e "======================================="
+echo -e "${ENDCOLOR}"
+
+echo "Reset trial & clear all data settings."
+read -p "Are you sure? (Y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Cleaning..."
+  # Backup
+  cp ~/.config/dconf/user ~/.config/dconf/user.bk
+  cp ~/.config/navicat/Premium/preferences.json ~/.config/navicat/Premium/preferences.json.bk
+  # Clear data in dconf
+  dconf reset -f /com/premiumsoft/navicat-premium/
+  # Remove data fields in config file
+  sed -i -E 's/,?"([A-Z0-9]+)":\{([^\}]+)},?//g' ~/.config/navicat/Premium/preferences.json
+  # Done
+  echo "Done."
+else
+  echo "Aborted."
+fi
